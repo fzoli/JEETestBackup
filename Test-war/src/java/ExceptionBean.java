0410111ed564c94ded646125b1307ca681a01a4e
zoli <f.zoli@mailbox.hu>
2014-06-23 07:38:04 +0200

    omnifaces exception handler test

---
+++ Test-war/src/java/ExceptionBean.java
@@ -0,0 +1,18 @@
+import java.sql.SQLException;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class ExceptionBean {
+
+    public void throwRuntimeException() {
+        throw new RuntimeException("peek-a-boo");
+    }
+
+    public void throwSQLException() throws SQLException {
+        throw new SQLException("DB fail");
+    }
+
+}
